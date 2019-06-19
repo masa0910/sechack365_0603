03c6079dccbc1fb25211c6502ec3ce469724ec4d
masahisa <masahisa0910@gmail.com>
2019-06-19 09:33:24 +0900

    add view template

--- SecHack365_0603/settings.py
+++ SecHack365_0603/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'SecHack365_0603.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

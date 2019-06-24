22dbbe05a238831684d14b1d210306083465e577
YoshikazuFusayasu <50490024+YoshikazuFusayasu@users.noreply.github.com>
2019-06-25 03:18:00 +0900

    Add files via upload

---
+++ db_access.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import django
+
+sys.path.append("d:\Desktop\sechack\sechack365_0603-master")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SecHack365_0603.settings')
+
+
+def call():
+    django.setup()
+    from sample.models import Usb
+    usb = []
+
+    usb_list = Usb.objects.all()
+    for obj in usb_list:
+        #print(obj)
+        usb.append(obj)
+
+    return usb
+
+if __name__ == '__main__':
+    call()
--- usb-test2.py
+++ usb-test2.py
@@ -3,20 +3,21 @@
 # で実行してください
 # DBとの連携とかは現状触ってないです、どう処理させましょう…?
 
-
 import win32com.client
 import time
 import sys
+import db_access
+
 
 #繋がっているデバイスのリストを更新
 def get_devinfo(dev_list, sta):
     #wmiを使ってdeviceidを取得
     wmi = win32com.client.GetObject ("winmgmts:")
     for usb in wmi.InstancesOf ("Win32_USBHub"):
-        if usb.DeviceID.split("\\") not in dev_list:
+        if usb.DeviceID.split("\\")[2] not in dev_list:
             #初期デバイスリストの作成(DB使うときは、ここにDBのリストを引っ張ってくる(今ある処理を消すと、ハブとかが引っかかるので注意))
             if sta == 0:
-                dev_list.append(usb.DeviceID.split("\\"))
+                dev_list.append(usb.DeviceID.split("\\")[2])
             #初回のリスト作成以外で新規のデバイスが見つかったとき
             else:
                 #新規デバイスを返り値とする
@@ -24,29 +25,31 @@ def get_devinfo(dev_list, sta):
 
 def main():
     dev_list = []               #新規デバイス検知用のリスト(既存のデバイスを抱えるための)
-    
+    for obj in db_access.call():
+        dev_list.append(str(obj))
     #初期デバイスリストの作成
     get_devinfo(dev_list, 0) 
+    print(str(dev_list))
     
     while 1:
         #デバイスリストの更新
         new_dev = get_devinfo(dev_list, 1)
         #Noneが帰ってくる = 新規デバイスなし/None以外が帰ってくる = 新規デバイスがnew_devに格納されている
         if new_dev != None:
-            print("new device : "+str(new_dev))
+            print("new device : "+str(new_dev[2]))
             #regモード(新規デバイス登録モード)起動時
             if sys.argv[1] == "reg":
                 permit_reg = input("allow ...add new device? (y or n) :" )
                 if permit_reg == "y":
                     dev_list.append(new_dev)
-                    print("add : new divice"+str(new_dev))
+                    print("add : new divice"+str(new_dev[2]))
                     sys.argv[1] = "det"
                     continue
             #detモード(新規デバイス検出)起動時
             if sys.argv[1] == "det":
                 ###何か新しいデバイスあるよ!!!っていうところ
                 ###他と組み合わせてエラー画面とか吐かせたいですね
-                print("not allow ...new divice : "+str(new_dev))
+                print("not allow ...new divice : "+str(new_dev[2]))
                 input()
         #次回処理までの待ち時間設定(sec)
         time.sleep(1)
